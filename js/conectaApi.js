3bcc706aae9c79866000a53c15978813b0347bf4
Wellington Fernandes <wellsfernandes1991@gmail.com>
2023-07-06 22:10:26 -0300

    Adiciona função de busca de vídeos por termo de pesquisa, exibindo os resultados em uma lista.
    Este commit adiciona uma nova função chamada buscaVideo que é acionada quando o usuário clica no botão de pesquisa. A função captura o termo de pesquisa digitado pelo usuário, realiza uma solicitação assíncrona para a API usando o módulo conectaApi.buscaVideo, exibe os resultados na lista de vídeos e permite uma experiência interativa de busca.

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -9,7 +9,7 @@ async function criaVideo(titulo, descricao, url, imagem) {
     const conexao = await fetch("http://localhost:3000/videos", {
         method: "POST",
         headers: {
-            "Content-type": "aplication/json"
+            "Content-type": "application/json"
         },
         body: JSON.stringify({
             titulo: titulo,
@@ -23,7 +23,15 @@ async function criaVideo(titulo, descricao, url, imagem) {
     return conexaoConvertida;
 }
 
+async function buscaVideo(termoDeBusca) {
+    const conexao = await fetch(`http://localhost:3000/videos?q=${termoDeBusca}`);
+    const conexaoConvertida = await conexao.json();
+
+    return conexaoConvertida;
+}
+
 export const conectaApi = {
     listaVideos,
-    criaVideo
+    criaVideo,
+    buscaVideo
 }
